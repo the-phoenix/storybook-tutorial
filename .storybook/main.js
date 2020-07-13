33da27faaf679c5e665e8cce930f7e0fc04dbb20
the-phoenix <9201924+the-phoenix@users.noreply.github.com>
2020-07-13 15:29:24 -0400

    add story book

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // stories: ['../src/**/*.stories.js'],
+  stories: ["../src/components/**/*.stories.js"],
+  addons: [
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+  ],
+};
