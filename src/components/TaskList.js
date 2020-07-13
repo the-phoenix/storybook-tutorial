f0d909c179b751786928ef8c0895c54c3e799902
the-phoenix <9201924+the-phoenix@users.noreply.github.com>
2020-07-13 16:06:54 -0400

    update empty comp log

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -39,7 +39,7 @@ export function PureTaskList({ loading, tasks, onPinTask, onArchiveTask }) {
       <div className="list-items">
         <div className="wrapper-message">
           <span className="icon-check" />
-          <div className="title-message">You have no tasks, indeed!</div>
+          <div className="title-message">You have no tasks!</div>
           <div className="subtitle-message">Sit back and relax</div>
         </div>
       </div>
